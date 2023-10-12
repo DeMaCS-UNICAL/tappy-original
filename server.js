f7591c9968720f2e5bcd15943cc8aeefbbd75c2f
deniseAngilica <deangilica@gmail.com>
2023-10-12 16:10:30 +0200

    headless server introduced

--- server.js
+++ server.js
@@ -2,13 +2,27 @@
 
 // Load all Libraries and Config
 var
+    config = require("./config"),
+    mode = 'brainybot1',
     five = require("johnny-five"),
     calibration = require("./lib/calibration"),
     Robot = require("./lib/robot").Robot,
     kinematics = require("./lib/kinematics"),
-    motion = require("./lib/motion"),
-    config = require("./config");
+    motion = require("./lib/motion");
 
+for(i=2; i<process.argv.length; i++){
+    val=process.argv[i];
+    console.log(val);
+    if(val.startsWith('--hardware'))
+    {
+        mode= val.split('=')[1];
+        break;
+    }
+    else
+    {
+        console.error("Unsupported option: "+val);
+    }
+}
 const Hapi = require('hapi');
 
 // Initialize HTTP Server
@@ -42,37 +56,49 @@ const init = async () => {
 	    console.log(`Server running at: ${server.info.uri}`);
 };
 init();
-
 // Initialize Socket.IO and make globally available
 global.io = require('socket.io')(server.listener);
+let serverImpl;
 
-// Initiliaze Johhny-Five Board
-var board = new five.Board({ debug: true, port: config.serialport || null });
-board.on("ready", function() {
-
-    // Initialize servos
-    var s1 = new five.Servo({ pin: config.s1.pin });
-    var s2 = new five.Servo({ pin: config.s2.pin });
-    var s3 = new five.Servo({ pin: config.s3.pin });
-
-    // Load calibration data
-    var calibrationData = calibration.getDataFromFilePath('calibration.json');
-
-    // Initialize kinematics
-    var k = new kinematics.Kinematics({
-        e: config.e,
-        f: config.f,
-        re: config.re,
-        rf: config.rf
-    });
-
-    var robot = new Robot(s1, s2, s3, calibrationData, k, config); // Initialize Robot instance
-    // var repl = require('./lib/repl')(board, robot); // Testing through command line
+if(mode=="headless"){
+    global.headless=true;
+    var robot = new Robot(); // Initialize Robot instance
+        // var repl = require('./lib/repl')(board, robot); // Testing through command line
     var rest = require('./lib/rest')(server, robot); // load REST API
-    var listeners = require('./lib/listeners')(io, robot, config, s1, s2, s3); // Lets Start socket Listeners
-    //server.start(); // And Finally Start HTTP server
+    var listeners = require('./lib/listeners')(io, robot, config); // Lets Start socket Listeners
+    global.ip = "127.0.0.1";
+}
+else if(mode=="brainybot1"){
+    var board = new five.Board({ debug: true, port: config.serialport || null });
+    board.on("ready", function() {
+    
+        // Initialize servos
+        var s1 = new five.Servo({ pin: config.s1.pin });
+        var s2 = new five.Servo({ pin: config.s2.pin });
+        var s3 = new five.Servo({ pin: config.s3.pin });
+    
+        // Load calibration data
+        var calibrationData = calibration.getDataFromFilePath('calibration.json');
+    
+        // Initialize kinematics
+        var k = new kinematics.Kinematics({
+            e: config.e,
+            f: config.f,
+            re: config.re,
+            rf: config.rf
+        });
+    
+        var robot = new Robot(s1, s2, s3, calibrationData, k, config); // Initialize Robot instance
+        // var repl = require('./lib/repl')(board, robot); // Testing through command line
+        var rest = require('./lib/rest')(server, robot); // load REST API
+        var listeners = require('./lib/listeners')(io, robot, config); // Lets Start socket Listeners
+        //server.start(); // And Finally Start HTTP server
+    
+            global.ip = "127.0.0.1";
+            console.log('Board ready');
+    
+    });
+}
+else if(mode=="brainybot2"){
 
-        global.ip = "127.0.0.1";
-        console.log('Board ready');
-
-});
+}
