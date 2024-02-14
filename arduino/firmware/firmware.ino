fba31007f40a669ce5ccfea056fed166a900e97e
Emanuele Galardo <galardo.emanuele@gmail.com>
2024-02-14 16:40:26 +0100

    FIRMWARE 2.0

--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -43,6 +43,9 @@ void compute() {
     dxl.setGoalPosition(1, value1, UNIT_DEGREE);
     dxl.setGoalPosition(2, value2, UNIT_DEGREE);
     dxl.setGoalPosition(3, value3, UNIT_DEGREE);
+
+    soft_serial.println("OK");
+    return;
     
   } else if(strncmp(inputBuffer, "GAP", 3) == 0){
     soft_serial.println(
@@ -50,27 +53,41 @@ void compute() {
       String(dxl.getPresentPosition(2, UNIT_DEGREE)) + " " + 
       String(dxl.getPresentPosition(3, UNIT_DEGREE))
       );
+    return;
+     
   } else if (strncmp(inputBuffer, "SSP", 3) == 0) {
     id = inputBuffer[4] - '0';
     float value = atof(inputBuffer + 6);
     dxl.setGoalPosition(id, value, UNIT_DEGREE);
+
+    soft_serial.println("OK");
+    return;
     
   } else if (strncmp(inputBuffer, "GSP", 3) == 0) {
     id = inputBuffer[4] - '0';
     soft_serial.println(dxl.getPresentPosition(id, UNIT_DEGREE));
+    return;
     
   } else if (strncmp(inputBuffer, "ISM", 3) == 0) {
     id = inputBuffer[4] - '0';
     soft_serial.println(dxl.readControlTableItem(MOVING, id));
+    return;
     
   } else if (strncmp(inputBuffer, "SPV", 3) == 0) {
     id = inputBuffer[4] - '0';
     uint8_t value = atoi(inputBuffer + 6); 
     dxl.writeControlTableItem(PROFILE_VELOCITY, id, value);
+
+    soft_serial.println("OK");
+    return;
+    
   } else if (strncmp(inputBuffer, "GPV", 3) == 0) {
     id = inputBuffer[4] - '0';
     soft_serial.println(dxl.readControlTableItem(PROFILE_VELOCITY, id));
+    return;
   }
+
+  soft_serial.println("ERR");
 }
 
 void loop() {
